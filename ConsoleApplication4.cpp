0b8afbe3c0b5a913304506e02998a88ab4c7175b
Lukmen1 <144219764+Lukmen1@users.noreply.github.com>
2023-11-08 12:07:58 +0300

    Plus C++

---
+++ ConsoleApplication4.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a{};
+    int b{};
+    cout << "Enter first number: ";
+    cin >> a;
+    cout << "Enter second number: ";
+    cin >> b;
+    int c = a + b;
+    cout << a << " + " << b << " = " << c;
+
+}
